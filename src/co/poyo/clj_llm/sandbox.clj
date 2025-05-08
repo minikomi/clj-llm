fe7c368997e12b6279ebbf5388bbffce06181078
Adam Moore <adam@poyo.co>
2025-05-08 17:53:48 +0900

    attachments

--- src/co/poyo/clj_llm/sandbox.clj
+++ src/co/poyo/clj_llm/sandbox.clj
@@ -14,27 +14,6 @@
   )
 
 
-(def transfer-money-schema
-  [:->
-   [:map [:from :string] [:to :string] [:amount :int]]
-   [:map
-    [:transaction-id :uuid]
-    [:details
-     [:map
-      [:from :string]
-      [:to :string]
-      [:amount :int]]]
-    [:status {:description "Transaction status"} :string]]])
-
-(defn transfer-money
-  {:malli/schema transfer-money-schema}
-  [{:keys [from to amount]}]
-  {:transaction-id (UUID/randomUUID)
-    :details {:from from
-                 :to to
-                 :amount amount}
-   :status "completed"})
-
 (comment
   (openai/register-backend!)
   (malli.instrument/instrument!)
@@ -49,7 +28,7 @@
       [:enum "celsius" "fahrenheit"]]])
 
   (def weather-tool
-    (sch/function-schema
+    (sch/function-schema->openai-tool-spec
      "get_weather"
      "Get the current weather in a location"
      weather-schema))
@@ -60,20 +39,51 @@
      (llm/prompt :openai/gpt-4.1-mini
                  "What's the weather like where The Eifel Tower is?"
                  :tools [weather-tool]
-                 :tool-choice "auto")
-     )
-   )
+                 :tool-choice "auto")))
+
+  @(:text (llm/prompt :openai/gpt-4.1-mini
+                      "what is this picture of?"
+                      {:attachments  [{:type :image
+                                       :url "https://images.vexels.com/media/users/3/128011/isolated/lists/527067b3541bd657cae7ce720cc3d301-hand-drawn-sitting-cat.png"
+                                       }]}))
 
   (reverse (:chunks (llm/prompt :openai/gpt-4.1-nano "hi there")))
 
-  (malli.instrument/instrument!)
 
-  (m/deref (get-in (m/function-schemas) ['co.poyo.clj-llm.sandbox 'transfer-money :schema]))
+  (do
 
-  (llm/call-function-with-llm
-   transfer-money
-   :openai/gpt-4.1-nano
-   "Transfer $50 from my savings account to my checking account")
+
+    (defn transfer-money3
+      {:malli/schema transfer-money-schema}
+      [{:keys [from to amount]}]
+      {:transaction-id (UUID/randomUUID)
+       :details {:from from
+                 :to to
+                 :amount amount}
+       :status "completed"})
+    (m/=>
+     transfer-money3
+     [:->
+      [:map [:from :string] [:to :string] [:amount :int]]
+      [:map
+       [:transaction-id :uuid]
+       [:details
+        [:map
+         [:from :string]
+         [:to :string]
+         [:amount :int]]]
+       [:status {:description "Transaction status"} :string]]]
+     )
+
+    (malli.instrument/collect!)
+
+    (llm/call-function-with-llm
+     transfer-money3
+     :openai/gpt-4.1-nano
+     "Transfer $50 from my savings account to my checking account")
+
+    )
+
 
   (m/function-schemas)
   )
