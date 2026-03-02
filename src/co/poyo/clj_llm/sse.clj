f1f00b74193e84191736a5ed21283491fa6d8a31
exe.dev user <exedev@cljllm.exe.xyz>
2026-03-02 01:58:23 +0000

    Simplify sse.clj: drop unused options, trim to 40 lines
    Remove buffer-size/parser-fn options nobody uses. Remove event: line
    special case (nil return handles it). Same parse-sse interface.
    
    Co-authored-by: Shelley <shelley@exe.dev>

--- src/co/poyo/clj_llm/sse.clj
+++ src/co/poyo/clj_llm/sse.clj
@@ -1,56 +1,44 @@
 (ns co.poyo.clj-llm.sse
-  "SSE stream parsing."
   (:require
    [cheshire.core :as json]
    [clojure.string :as str]
-   [clojure.core.async :as a :refer [chan >!! close! thread]]
+   [clojure.core.async :refer [chan >!! close! thread]]
    [clojure.java.io :as io]
    [clojure.walk :as walk]
    [camel-snake-kebab.core :as csk]))
 
-(defn- parse-json-kebab
-  "Parse JSON string, keywordize with kebab-case."
-  [s]
+(defn- parse-json-kebab [s]
   (->> (json/parse-string s)
        (walk/postwalk (fn [x]
                         (if (map? x)
                           (update-keys x (comp keyword csk/->kebab-case))
                           x)))))
 
-(defn- parse-sse-line
-  "Parse a single SSE line. Returns a map or nil.
-   Data lines → {::data parsed}, done → {::done true}, others → nil."
-  [line]
-  (cond
-    (str/starts-with? line "event:") nil
-    (str/starts-with? line "data:")
+(defn- parse-line [line]
+  (when (str/starts-with? line "data:")
     (let [raw (str/trim (subs line 5))]
       (if (= raw "[DONE]")
         {::done true}
         (try {::data (parse-json-kebab raw)}
-             (catch Exception _ nil))))
-    :else nil))
+             (catch Exception _ nil))))))
 
 (defn parse-sse
-  "Parse an SSE InputStream into a channel of event maps.
-   Returns channel of {::data ...}, {::done true}, or {::error ...}."
-  ([input-stream] (parse-sse input-stream {}))
-  ([input-stream {:keys [buffer-size parser-fn]
-                  :or {buffer-size 1024
-                       parser-fn parse-sse-line}}]
-   (let [out (chan buffer-size)]
-     (thread
-       (try
-         (with-open [reader (io/reader input-stream)]
-           (loop []
-             (when-let [line (.readLine reader)]
-               (if-let [event (parser-fn line)]
-                 (do (>!! out event)
-                     (when-not (::done event)
-                       (recur)))
-                 (recur)))))
-         (catch Exception e
-           (>!! out {::error e}))
-         (finally
-           (close! out))))
-     out)))
+  "Parse an SSE InputStream into a channel.
+   Events are {::data map}, {::done true}, or {::error ex}."
+  [input-stream]
+  (let [out (chan 1024)]
+    (thread
+      (try
+        (with-open [reader (io/reader input-stream)]
+          (loop []
+            (when-let [line (.readLine reader)]
+              (if-let [event (parse-line line)]
+                (do (>!! out event)
+                    (when-not (::done event)
+                      (recur)))
+                (recur)))))
+        (catch Exception e
+          (>!! out {::error e}))
+        (finally
+          (close! out))))
+    out))
